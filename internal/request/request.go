bdb71604a59f9fa3db7a238fd3197a5b79d0a495
PavelVaavra <pavelvavra.vm@gmail.com>
2025-08-12 18:47:09 +0200

    parsing the request line finished

--- internal/request/request.go
+++ internal/request/request.go
@@ -2,6 +2,9 @@ package request
 
 import (
 	"io"
+	"log"
+	"strings"
+	"errors"
 )
 
 type Request struct {
@@ -15,5 +18,45 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	return nil, nil
+	b, err := io.ReadAll(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	parts := strings.Split(string(b), "\r\n")
+	
+	rl, err := parseRequestLine(parts[0])
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{ RequestLine: *rl }, nil
+}
+
+func parseRequestLine(s string) (*RequestLine, error) {
+	parts := strings.Split(s, " ")
+	if len(parts) != 3 {
+		return nil, errors.New("Request line doesn't consist of three parts.")
+	}
+	method := parts[0]
+	for _, c := range method {
+		if c < 'A' || c > 'Z' {
+			return nil, errors.New("Method contains other than capital letters.")
+		}
+	}
+	if method != "GET" && method != "POST" && method != "PUT" && method != "DELETE" {
+		return nil, errors.New("Not a valid method.")
+	}
+
+	requestTarget := parts[1]
+
+	httpVersion := strings.Split(parts[2], "/")[1]
+	if httpVersion != "1.1" {
+		return nil, errors.New("HTTP version not equal to 1.1")
+	}
+
+	return &RequestLine{
+		HttpVersion: httpVersion,
+		RequestTarget: requestTarget,
+		Method: method,
+	}, nil
 }
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -8,8 +8,6 @@ import (
 )
 
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
-
 	// Test: Good GET Request line
 	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.NoError(t, err)
@@ -26,7 +24,31 @@ func TestRequestLineParse(t *testing.T) {
 	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
 	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
+	// Test: Good POST Request line with path
+	r, err = RequestFromReader(strings.NewReader("POST /tea HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/tea", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
 	// Test: Invalid number of parts in request line
 	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
+	assert.Equal(t, "Request line doesn't consist of three parts.", err.Error())
+
+	// Test: Method doesn't contain only capital letters
+	_, err = RequestFromReader(strings.NewReader("get /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+	assert.Equal(t, "Method contains other than capital letters.", err.Error())
+
+	// Test: Invalid method (out of order) Request line
+	_, err = RequestFromReader(strings.NewReader("GETT /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+	assert.Equal(t, "Not a valid method.", err.Error())
+
+	// Test: Invalid version in Request line
+	_, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.2\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+	assert.Equal(t, "HTTP version not equal to 1.1", err.Error())
 }
