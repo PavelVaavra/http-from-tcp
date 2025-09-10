b74ae1b0b543f1d50eeeac5889b05e2c2c192b65
PavelVaavra <pavelvavra.vm@gmail.com>
2025-09-10 19:45:48 +0200

    Writer struct refactor

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 
+	"github.com/PavelVaavra/http-from-tcp/internal/headers"
 	"github.com/PavelVaavra/http-from-tcp/internal/request"
 	"github.com/PavelVaavra/http-from-tcp/internal/response"
 	"github.com/PavelVaavra/http-from-tcp/internal/server"
@@ -31,22 +34,35 @@ func textHandler(w *response.Writer, req *request.Request) {
 	switch req.RequestLine.RequestTarget {
 	case "/yourproblem":
 		w.StatusCode = response.StatusCodeBadRequest
-		w.Message = "Your problem is not my problem\n"
+		w.StatusPhrase = "Bad Request"
+		w.BodyText = "Your problem is not my problem\n"
 	case "/myproblem":
 		w.StatusCode = response.StatusCodeInternalServerError
-		w.Message = "Woopsie, my bad\n"
+		w.StatusPhrase = "Internal Server Error"
+		w.BodyText = "Woopsie, my bad\n"
 	default:
 		w.StatusCode = response.StatusCodeOK
-		w.Message = "All good, frfr\n"
+		w.StatusPhrase = "OK"
+		w.BodyText = "All good, frfr\n"
 	}
-	w.Write()
+
+	w.Headers = headers.Headers{
+		"Connection":     "close",
+		"Content-Length": fmt.Sprintf("%v", len(w.BodyText)),
+		"Content-Type":   "text/plain",
+	}
+
+	w.WriteStatusLine()
+	w.WriteHeaders()
+	w.WriteBody()
 }
 
 func htmlHandler(w *response.Writer, req *request.Request) {
 	switch req.RequestLine.RequestTarget {
 	case "/yourproblem":
 		w.StatusCode = response.StatusCodeBadRequest
-		w.Message = `<html>
+		w.StatusPhrase = "Bad Request"
+		w.BodyText = fmt.Sprintf(`<html>
   <head>
     <title>%s %s</title>
   </head>
@@ -54,10 +70,11 @@ func htmlHandler(w *response.Writer, req *request.Request) {
     <h1>%s</h1>
     <p>Your request honestly kinda sucked.</p>
   </body>
-</html>`
+</html>`, strconv.Itoa(int(w.StatusCode)), w.StatusPhrase, w.StatusPhrase)
 	case "/myproblem":
 		w.StatusCode = response.StatusCodeInternalServerError
-		w.Message = `<html>
+		w.StatusPhrase = "Internal Server Error"
+		w.BodyText = fmt.Sprintf(`<html>
   <head>
     <title>%s %s</title>
   </head>
@@ -65,10 +82,11 @@ func htmlHandler(w *response.Writer, req *request.Request) {
     <h1>%s</h1>
     <p>Okay, you know what? This one is on me.</p>
   </body>
-</html>`
+</html>`, strconv.Itoa(int(w.StatusCode)), w.StatusPhrase, w.StatusPhrase)
 	default:
 		w.StatusCode = response.StatusCodeOK
-		w.Message = `<html>
+		w.StatusPhrase = "OK"
+		w.BodyText = fmt.Sprintf(`<html>
   <head>
     <title>%s %s</title>
   </head>
@@ -76,7 +94,63 @@ func htmlHandler(w *response.Writer, req *request.Request) {
     <h1>%s</h1>
     <p>Your request was an absolute banger.</p>
   </body>
-</html>`
+</html>`, strconv.Itoa(int(w.StatusCode)), w.StatusPhrase, w.StatusPhrase)
 	}
-	w.Write()
+
+	w.Headers = headers.Headers{
+		"Connection":     "close",
+		"Content-Length": fmt.Sprintf("%v", len(w.BodyText)),
+		"Content-Type":   "text/html",
+	}
+
+	w.WriteStatusLine()
+	w.WriteHeaders()
+	w.WriteBody()
 }
+
+// func chunkHandler(w *response.Writer, req *request.Request) {
+// 	url := "https://httpbin.org"
+// 	if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin") {
+// 		url += strings.TrimPrefix(req.RequestLine.RequestTarget, "/httpbin")
+// 	}
+
+// 	res, err := http.Get(url)
+// 	if err != nil {
+// 		fmt.Errorf("http.Get(\"%v\" err - %v\n", url, err.Error())
+// 	}
+// 	defer res.Body.Close()
+
+// 	w.StatusCode = response.StatusCode(res.StatusCode)
+
+// 	w.WriteChunked(res.Body)
+// }
+
+// Add a new proxy handler to your server that maps /httpbin/x to https://httpbin.org/x, supporting both proxying and chunked responsing.
+
+// 	I used the strings.HasPrefix and strings.TrimPrefix functions to handle routing and route parsing.
+
+// 	Be sure to remove the Content-Length header from the response, and add the Transfer-Encoding: chunked header.
+
+// 	I used http.Get to make the request to httpbin.org and resp.Body.Read to read the response body. I used a buffer size of 1024 bytes, and then
+// 	printed n on every call to Read so that I could see how much data was being read. Use n as your chunk size and write that chunk data back to the
+// 	client as soon as you get it from httpbin.org. It's pretty cool to see how the data can be forwarded in real-time!
+
+// 	I recommend using netcat to test your chunked responses. Curl will abstract away the chunking for you, so you won't see your hex and cr and lf
+// 	characters in your terminal if you use curl. I used this command to see my raw chunked response:
+
+// echo -e "GET /httpbin/stream/100 HTTP/1.1\r\nHost: localhost:42069\r\nConnection: close\r\n\r\n" | nc localhost 42069
+
+// GET localhost:42069/httpbin/stream/100 will trigger a handler on our server that sends a request to https://httpbin.org/stream/100
+// and then forwards the response back to the client chunk by chunk.
+
+// HTTP/1.1 200 OK
+// Content-Type: text/plain
+// Transfer-Encoding: chunked
+
+// 1E
+// I could go for a cup of coffee
+// B
+// But not Java
+// 12
+// Never go full Java
+// 0
--- internal/response/response.go
+++ internal/response/response.go
@@ -1,50 +1,12 @@
 package response
 
 import (
-	"fmt"
 	"io"
 	"net"
 	"strconv"
-	"strings"
 
 	"github.com/PavelVaavra/http-from-tcp/internal/headers"
 )
-
-type Writer struct {
-	StatusCode StatusCode
-	Message    string
-	Conn       net.Conn
-}
-
-func (w *Writer) Write() {
-	statusCodeStr := strconv.Itoa(int(w.StatusCode))
-	statusPhraseStr := statusPhrase[statusCodeStr]
-
-	err := WriteStatusLine(w.Conn, statusCodeStr, statusPhraseStr)
-	if err != nil {
-		return
-	}
-
-	isHtml := strings.Contains(w.Message, "<html>")
-
-	contentType := "plain"
-	if isHtml {
-		contentType = "html"
-		w.Message = fmt.Sprintf(w.Message, statusCodeStr, statusPhraseStr, statusPhraseStr)
-	}
-
-	headers := GetDefaultHeaders(len(w.Message), contentType)
-
-	err = WriteHeaders(w.Conn, headers)
-	if err != nil {
-		return
-	}
-
-	_, err = w.Conn.Write([]byte(w.Message))
-	if err != nil {
-		return
-	}
-}
 
 type StatusCode int
 
@@ -54,38 +16,38 @@ const (
 	StatusCodeInternalServerError StatusCode = 500
 )
 
-var statusPhrase = map[string]string{
-	"200": "OK",
-	"400": "Bad Request",
-	"500": "Internal Server Error",
+type Writer struct {
+	StatusCode   StatusCode
+	StatusPhrase string
+	Headers      headers.Headers
+	BodyText     string
+	BodyChunked  io.ReadCloser
+	Trailers     headers.Headers
+	Conn         net.Conn
 }
 
-func WriteStatusLine(w io.Writer, statusCode string, statusPhrase string) error {
-	statusLine := "HTTP/1.1 " + statusCode + " " + statusPhrase + "\r\n"
-	_, err := w.Write([]byte(statusLine))
+func (w *Writer) WriteStatusLine() error {
+	statusLine := "HTTP/1.1 " + strconv.Itoa(int(w.StatusCode)) + " " + w.StatusPhrase + "\r\n"
+	_, err := w.Conn.Write([]byte(statusLine))
 	return err
 }
 
-func GetDefaultHeaders(contentLen int, contentType string) headers.Headers {
-	headers := headers.Headers{}
-	headers.Set("Content-Length", fmt.Sprintf("%v", contentLen))
-	headers.Set("Connection", "close")
-	headers.Set("Content-Type", fmt.Sprintf("text/%v", contentType))
-
-	return headers
-}
-
-func WriteHeaders(w io.Writer, headers headers.Headers) error {
-	for k, v := range headers {
+func (w *Writer) WriteHeaders() error {
+	for k, v := range w.Headers {
 		header := k + ": " + v + "\r\n"
-		_, err := w.Write([]byte(header))
+		_, err := w.Conn.Write([]byte(header))
 		if err != nil {
 			return err
 		}
 	}
-	_, err := w.Write([]byte("\r\n"))
+	_, err := w.Conn.Write([]byte("\r\n"))
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func (w *Writer) WriteBody() error {
+	_, err := w.Conn.Write([]byte(w.BodyText))
+	return err
 }
